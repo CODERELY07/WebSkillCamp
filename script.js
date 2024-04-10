d6a7e86077d264ba734d3701367d0dc493715157
Mark Ely Calipjo <calipjo.markely@gmail.com>
2024-04-10 16:26:18 +0800

    withcareerpage

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ burger.onclick = function (){
     let nav = document.querySelector('nav');
     nav.classList.toggle('active');
 }
+// up
+const up = document.getElementById('up');
 
+up.onclick = function() {
+    if(up.classList.contains('fa-chevron-down')) {
+        up.classList.remove('fa-chevron-down');
+        up.classList.add('fa-arrow-up');
+    }
+};
 // login
 
 let loginBtn = document.getElementById('login');
@@ -17,5 +25,7 @@ loginBtn.addEventListener("click",()=>{
 signup.addEventListener("click",()=>{
     window.location.href = 'signup.html';
 })
+
+
 
 
